bbd8d9de36544a68d2d1fb5116cec55ff09cd6cb
darkmodz427 <78821754+darkmodz427@users.noreply.github.com>
2021-03-10 18:16:22 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Hyka*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.hykmenu = toinmenu
